2bcf9a7945e94319b90829853742d6193e62710f
Юлия Нуреева <julianureevs@gmail.com>
2023-10-08 18:25:15 +0300

    add hello.pas

---
+++ hello.pas
@@ -0,0 +1,10 @@
+﻿var language: string;
+begin
+  writeln('Выберете язык: русский, французский, английский');
+  readln(language);
+  case language of
+    'русский': print('привет!');
+    'французский': print('bonjour!');
+    'английский': print('hello!');
+  end;
+end.
